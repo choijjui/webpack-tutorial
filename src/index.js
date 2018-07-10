10e0d837d5900a0b322293b61e123de64a58c55e
choijjui <choijunah@gmail.com>
2018-07-10 22:37:00 +0900

    webpack 'Asset Management' complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from "lodash";
+import './style.css';
+import Icon from './webpack.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
+
+    var webpackIcon = new Image();
+    webpackIcon.src = Icon;
+
+    element.appendChild(webpackIcon);
+
+    console.log(Data);
   
     return element;
   }
